e7d673434b0e603b52c5564403bf9e5c8f39004c
ChrisPCruz99 <christian.pcruz99@gmail.com>
2023-02-20 17:15:45 -0300

    New: Criação do projeto

---
+++ src/main/java/br/com/carteiradoze/CarteiraDoZeApplication.java
@@ -0,0 +1,13 @@
+package br.com.carteiradoze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraDoZeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraDoZeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carteiradoze/CarteiraDoZeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carteiradoze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraDoZeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
