883eb9eea68fd2badc66a0c9ce86183ad4ac80df
ChrisPCruz99 <christian.pcruz99@gmail.com>
2023-02-20 17:41:13 -0300

    New: Criação da model Quilometragem

---
+++ src/main/java/br/com/carteiradoze/model/Quilometragem.java
@@ -0,0 +1,53 @@
+package br.com.carteiradoze.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_quilometragem")
+public class Quilometragem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O campo quantidade é obrigatório")
+	public BigDecimal quantidade;
+	
+   /* @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movimentacao_id")
+    private Movimentacao movimentacao;
+
+	public Movimentacao getMovimentacao() {
+		return movimentacao;
+	}
+
+	public void setMovimentacao(Movimentacao movimentacao) {
+		this.movimentacao = movimentacao;
+	}*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+}
