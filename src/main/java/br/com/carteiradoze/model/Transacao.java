33fd23569ebca0db2fb58a3a51e415320749399e
ChrisPCruz99 <christian.pcruz99@gmail.com>
2023-02-20 17:39:03 -0300

    New: Criação da model Transacao

---
+++ src/main/java/br/com/carteiradoze/model/Transacao.java
@@ -0,0 +1,46 @@
+package br.com.carteiradoze.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_transacao")
+public class Transacao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O campo tipo é obrigatório")
+	public String tipo;
+	
+	/*@OneToMany(mappedBy = "transacao", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("transacao")
+	private List<Movimentacao> movimentacao;*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
