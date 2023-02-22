360add81119cc526247882aff57f1f20ea2c52a7
ChrisPCruz99 <christian.pcruz99@gmail.com>
2023-02-22 09:54:46 -0300

    New: Criação da interface MovimentacaoRepository

---
+++ src/main/java/br/com/carteiradoze/repository/MovimentacaoRepository.java
@@ -0,0 +1,19 @@
+package br.com.carteiradoze.repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.carteiradoze.model.Movimentacao;
+
+
+@Repository
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
+	public List<Movimentacao> findAllByValor(@Param("valor") BigDecimal valor);
+	public List<Movimentacao> findAllByDataTransacao(@Param("data") LocalDateTime data);
+
+}
