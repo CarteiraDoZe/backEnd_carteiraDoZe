2f6c77de65078cca117adf9d417fa4238a3f61ff
ChrisPCruz99 <christian.pcruz99@gmail.com>
2023-03-07 13:30:49 -0300

    New: Criação da camada de basic security e do UsuarioController

---
+++ src/main/java/br/com/carteiradoze/controller/QuilometragemController.java
@@ -0,0 +1,5 @@
+package br.com.carteiradoze.controller;
+
+public class QuilometragemController {
+
+}
---
+++ src/main/java/br/com/carteiradoze/model/UsuarioLogin.java
@@ -0,0 +1,62 @@
+package br.com.carteiradoze.model;
+
+import java.time.LocalDate;
+
+public class UsuarioLogin {
+	
+	private Long id;
+	private String nome;
+	private String usuario;
+	private LocalDate dataNasc;
+	private String senha;
+	private String token;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public LocalDate getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(LocalDate dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public String getToken() {
+		return this.token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
